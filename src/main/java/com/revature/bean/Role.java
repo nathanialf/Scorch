cba2ab5d70bb8b6beb7958a4780bd0dd5118b5ce
Nathanial Fine <nathanial.fine@gmail.com>
2017-07-07 22:53:28 -0400

    Add Role bean

--- src/main/java/com/revature/bean/Role.java
+++ src/main/java/com/revature/bean/Role.java
@@ -1,5 +1,57 @@
 package com.revature.bean;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity
+@Table(name="ROLE")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE, region="tapCache")
 public class Role {
 
+	@Id
+	@Column(name="ROLE_ID")
+	@SequenceGenerator(name="ROLEID_SEQ", sequenceName="ROLEID_SEQ")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="ROLEID_SEQ")
+	private int id;
+	
+	@Column(name="ROLE_NAME")
+	private String name;
+
+	public Role(){
+		super();
+	}
+	
+	public Role(String name) {
+		super();
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", name=" + name + "]";
+	}
 }
