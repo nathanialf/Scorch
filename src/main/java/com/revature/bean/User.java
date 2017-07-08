cb0d0956ec5f3385cdc835c8dc2782e5db690585
Nathanial Fine <nathanial.fine@gmail.com>
2017-07-07 21:06:58 -0400

    Added Hibernate and Setup User class

---
+++ src/main/java/com/revature/bean/User.java
@@ -0,0 +1,123 @@
+package com.revature.bean;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity
+@Table(name = "USER")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE, region="tapCache")
+public class User {
+	
+	@Id
+	@Column(name="USER_ID")
+	@SequenceGenerator(name="USERID_SEQ", sequenceName="USERID_SEQ")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="USERID_SEQ")
+	private int id;
+	
+	@Column(name="USER_USERNAME")
+	private String username;
+	
+	@Column(name="USER_PASSWORD")
+	private String password;
+	
+	@Column(name="USER_FIRSTNAME")
+	private String firstname;
+	
+	@Column(name="USER_LASTNAME")
+	private String lastname;
+	
+	@Column(name="USER_ACTIVE")
+	private int active;
+	
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinColumn(name="USER_ROLE")
+	private Role role;
+	
+	public User(){
+		super();
+	}
+
+	public User(String username, String password, String firstname, String lastname, int active, Role role) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.active = active;
+		this.role = role;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public int getActive() {
+		return active;
+	}
+
+	public void setActive(int active) {
+		this.active = active;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", firstname=" + firstname
+				+ ", lastname=" + lastname + ", active=" + active + ", role=" + role + "]";
+	}	
+}
