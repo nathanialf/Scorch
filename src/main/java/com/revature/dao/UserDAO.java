2ce304ac86c9f8028f36f7f7e9ddce7d418c2c93
Derek <derek.sirp@yahoo.com>
2017-07-08 21:46:21 -0400

    User DAO

--- src/main/java/com/revature/dao/UserDAO.java
+++ src/main/java/com/revature/dao/UserDAO.java
@@ -1,5 +1,14 @@
 package com.revature.dao;
 
-public interface UserDAO {
+import java.util.List;
 
+import com.revature.bean.User;
+
+public interface UserDAO {
+	
+	public int insertUser(User u);
+	public User getUserById(Integer id);
+	public List<User> getAllUsers();
+	public void setUser(User u);
+	public void deleteUser(Integer id);
 }
---
+++ src/main/java/com/revature/dao/UserDAOImpl.java
@@ -0,0 +1,134 @@
+package com.revature.dao;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.bean.User;
+import com.revature.util.HibernateUtil;
+
+public class UserDAOImpl implements UserDAO {
+	
+	@Override
+	public int insertUser(User user) {
+		Session session = HibernateUtil.getSession();
+		Transaction tx = null;
+		Integer userId = null;
+		
+		try {
+			tx = session.beginTransaction();
+			userId = (Integer)session.save(user);
+			tx.commit();
+		}
+		catch(HibernateException e) {
+			if(tx != null) {
+            	tx.rollback();
+            }
+            e.printStackTrace();
+        }
+        finally {
+        	session.close();
+        }
+        
+        return userId;
+		}
+
+	@Override
+	public User getUserById(Integer id) {
+		Session session = HibernateUtil.getSession();
+        Transaction tx = null;
+        User user = null;
+        
+        try {
+            tx = session.beginTransaction();
+            user = (User)session.get(User.class, id);
+            
+        }
+        catch(HibernateException e) {
+            if(tx != null) {
+            	tx.rollback();
+            }
+            e.printStackTrace();
+        }
+        finally {
+        	session.close();
+        }
+        
+        return user;
+	}
+
+	@Override
+	public List<User> getAllUsers() {
+		List<User> users = null;
+		Session session = HibernateUtil.getSession();
+		Transaction tx = null;
+		
+		try {
+	            tx = session.beginTransaction();
+		        users = session.createQuery("FROM Employee").list();
+	            
+	        }
+	        catch(HibernateException e) {
+	            if(tx != null) {
+	            	tx.rollback();
+	            }
+	            e.printStackTrace();
+	        }
+	        finally {
+	        	session.close();
+	        }
+	        
+	     return users;
+	}
+
+	@Override
+	public void setUser(User user) {
+		
+		Session session = HibernateUtil.getSession();
+        Transaction tx = null;
+        
+        try {
+            tx = session.beginTransaction();
+            user = (User)session.get(User.class, user.getId());
+            tx.commit();
+            
+        }
+        catch(HibernateException e) {
+            if(tx != null) {
+            	tx.rollback();
+            }
+            e.printStackTrace();
+        }
+        finally {
+        	session.close();
+        }
+	}
+
+	@Override
+	public void deleteUser(Integer id) {
+		
+		Session session = HibernateUtil.getSession();
+        Transaction tx = null;
+        User user = null;
+        
+        try {
+            tx = session.beginTransaction();
+            user = (User)session.get(User.class, id);
+            session.delete(user);
+            tx.commit();
+            
+        }
+        catch(HibernateException e) {
+            if(tx != null) {
+            	tx.rollback();
+            }
+            e.printStackTrace();
+        }
+        finally {
+        	session.close();
+        }
+	}
+
+}
