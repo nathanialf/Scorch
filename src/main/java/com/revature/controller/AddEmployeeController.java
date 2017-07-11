663749a4c7fef238cbf794dce96001f49c34353c
Nathanial Fine <nathanial.fine@gmail.com>
2017-07-11 19:53:09 -0400

    creating users works again

--- src/main/java/com/revature/controller/AddEmployeeController.java
+++ src/main/java/com/revature/controller/AddEmployeeController.java
@@ -1,6 +1,7 @@
 package com.revature.controller;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,37 +9,30 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.revature.bean.Role;
 import com.revature.bean.User;
 import com.revature.service.UserService;
 
 @Controller
-@RequestMapping(value = "/add")
-public class InsertEmployeeController {
-	User sessionUser;
+@RequestMapping(value = "/employee/add")
+public class AddEmployeeController {
 
 	@Autowired
 	UserService userService;
 
 	@RequestMapping(method = RequestMethod.POST)
-	public @ResponseBody String getEmployeesPost(User user, @RequestParam("role") int role, BindingResult bindingResult, ModelMap modelMap, HttpSession session) {
+	public String getEmployeesPost(@Valid User user, BindingResult bindingResult, ModelMap modelMap, HttpSession session) {
 		user.setActive(1);
 		user.setPassword("generic");
-		//System.out.println("\n\n\n" + role + "\n\n\n");
-		Role r = userService.getRole(role);
 		userService.addEmployee(user);
-		return "home";
+		return "newemployee";
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String getEmployeesGet(User user,@RequestParam("role") int role, BindingResult bindingResult, ModelMap modelMap, HttpSession session) {
+	public String getEmployeesGet(@Valid User user, BindingResult bindingResult, ModelMap modelMap, HttpSession session) {
 		user.setActive(1);
 		user.setPassword("generic");
-		Role r = userService.getRole(role);
 		userService.addEmployee(user);
-		return "home";
+		return "newemployee";
 	}
 }
