4b1f1dd5a16cdcd1e423402110a2ca10722883e4
Nathanial Fine <nathanial.fine@gmail.com>
2017-07-11 10:01:10 -0400

    Started batch controller

---
+++ src/main/java/com/revature/controller/BatchController.java
@@ -0,0 +1,42 @@
+package com.revature.controller;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.revature.bean.Batch;
+import com.revature.bean.User;
+
+@Controller
+@RequestMapping(value="/batch")
+public class BatchController {
+	
+	@Autowired
+	Batch emptyBatch;
+	
+	
+	@ModelAttribute("Some info")
+	public String addInfotoRequestScope(){
+		System.out.println("Adding info the the modelMap");
+		return "This is added information";
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String getLoginPage(ModelMap modelMap){
+		
+		return "batch";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String doLogin(@Valid User user, BindingResult bindingResult, ModelMap modelMap, HttpSession session){
+		
+		return "batch";
+	}
+}
